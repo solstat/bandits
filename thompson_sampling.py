1bd08f2a91893bf095c4b6a36cb6478ab5dabaee
kyleclo <kyleclo@uw.edu>
2017-03-05 18:13:12 -0800

    add memory multiplier to thompson sampling gaussian strategy and included dynamic bandit in example

--- thompson_sampling.py
+++ thompson_sampling.py
@@ -7,7 +7,7 @@ Thompson sampling example
 import numpy as np
 
 from arm import BernoulliArm, GaussianArm
-from bandit import StaticBandit
+from bandit import StaticBandit, LinearInterpolationBandit
 from strategy import ThompsonBernoulli, ThompsonGaussianKnownSigma
 
 if __name__ == '__main__':
@@ -16,25 +16,34 @@ if __name__ == '__main__':
     # Bernoulli
     bernoulli_bandit = StaticBandit(arms=[BernoulliArm(prob=0.45),
                                           BernoulliArm(prob=0.55)])
-    thompson_bernoulli = ThompsonBernoulli(bandit=bernoulli_bandit,
+    bernoulli_strategy = ThompsonBernoulli(bandit=bernoulli_bandit,
                                            alpha_prior=1, beta_prior=1)
-    result = thompson_bernoulli.fit(iterations=1000)
-    print('Prior params: ' + str(thompson_bernoulli.prior_params))
-    print('Posterior params: ' + str(thompson_bernoulli.posterior_params))
+    result = bernoulli_strategy.fit(iterations=1000)
+    print('Prior params: ' + str(bernoulli_strategy.prior_params))
+    print('Posterior params: ' + str(bernoulli_strategy.posterior_params))
     print('True means: ' + str([0.45, 0.55]))
-    print('Mean reward estimates: ' + str(thompson_bernoulli.mean_reward_estimates))
+    print('Mean reward estimates: ' + str(bernoulli_strategy.mean_reward_estimates))
 
     # Gaussian
     gaussian_bandit = StaticBandit(arms=[GaussianArm(mu=95, sigma=30),
                                          GaussianArm(mu=105, sigma=30)])
-    gaussian_bandit.pull_arm(1)
-
-    thompson_gaussian = ThompsonGaussianKnownSigma(bandit=gaussian_bandit,
+    gaussian_strategy = ThompsonGaussianKnownSigma(bandit=gaussian_bandit,
                                                    sigma=20,
                                                    mu_prior=0, sigma_prior=200)
-    thompson_gaussian.fit(iterations=500)
-    print('Prior params: ' + str(thompson_gaussian.prior_params))
-    print('Posterior params: ' + str(thompson_gaussian.posterior_params))
+    gaussian_strategy.fit(iterations=500)
+    print('Prior params: ' + str(gaussian_strategy.prior_params))
+    print('Posterior params: ' + str(gaussian_strategy.posterior_params))
     print('True means: ' + str([95, 105]))
-    print('Mean reward estimates: ' + str(thompson_gaussian.mean_reward_estimates))
+    print('Mean reward estimates: ' + str(gaussian_strategy.mean_reward_estimates))
+
+    # Linear interpolation bandit with Gaussian errors
+    dynamic_bandit = LinearInterpolationBandit(means=np.array([[5.0, 8.0], [10.0, 5.0]]),
+                                               periods=[200, 200])
+    dynamic_strategy = ThompsonGaussianKnownSigma(bandit=dynamic_bandit,
+                                                  sigma=20,
+                                                  mu_prior=0, sigma_prior=200,
+                                                  memory_multiplier=0.9)
+    dynamic_strategy.fit(iterations=1000, plot=True)
+    print('Prior params: ' + str(dynamic_strategy.prior_params))
+    print('Posterior params: ' + str(dynamic_strategy.posterior_params))
 
